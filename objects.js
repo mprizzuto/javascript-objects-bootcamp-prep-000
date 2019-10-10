f82c48868b9fae80ed9bb2d3c0b4180a9e0de4a1
MARCO RIZZUTO <mprizzuto88@gmail.com>
2019-10-10 13:12:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,6 @@
-var playlist= artist:'song'
+var playlist= {artist:'song' };
+
+Function updatePlaylist(playlist,'artistName',songTitle) { Object.assign(
+ {},playlist, {artist:['song']})
+return playlist;
+} 
